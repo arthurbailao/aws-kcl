73b9b65168e8cd5b691af46fde9e9960b5c21708
Arthur Bailão <arthurbailao@gmail.com>
2019-03-21 15:06:54 -0300

    writing first version of docs

--- record_processor.go
+++ record_processor.go
@@ -2,12 +2,16 @@ package kcl
 
 // RecordProcessor ...
 type RecordProcessor interface {
+	// Initialize is called once by the KCL before any calls to processRecords. Any initialization logic for record processing can go here.
 	Initialize(string) error
 
+	// ProcessRecords is called by KCL with a list of records to be processed and checkpointed. The checkpointer can optionally be used to checkpoint a particular sequence number (from a record).
 	ProcessRecords([]*Record, *Checkpointer) error
 
+	// ShutdownRequested is called by KCL to indicate that this record processor should shut down. After the shutdown requested operation is complete, there will not be any more calls to any other functions of this record processor. Clients should not attempt to checkpoint because the lease has been lost by this Worker.
 	ShutdownRequested(*Checkpointer) error
 
+	// Shutdown is called by the KCL to indicate that this record processor should shutdown. After the shutdown operation is complete, there will not be any more calls to any other functions of this record processor. Clients are required to checkpoint at this time. This indicates that the current record processor has finished processing and new record processors for the children will be created.
 	Shutdown(ShutdownType, *Checkpointer) error
 }
 
